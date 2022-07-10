6e673d2cb067606c309301a70b5a5c6060e364e0
TaeVon Lewis <lewistpro@gmail.com>
2022-07-10 14:02:19 -0400

    Added arrays.cpp

---
+++ hackerrank/arrays.cpp
@@ -0,0 +1,20 @@
+// Arrays Introduction
+
+#include <iostream>
+
+int main() {
+    int n{};
+    std::cin >> n; 
+    int arr[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+    }
+    
+    for (int i = n - 1; i >= 0; i--)
+    {
+        std::cout << arr[i] << " ";
+    }
+    return 0;
+}
