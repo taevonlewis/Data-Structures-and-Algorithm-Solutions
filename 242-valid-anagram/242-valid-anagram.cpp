772e465be873068a18c0d1ec37c6bebfc9b76002
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-09-06 15:06:50 -0400

    Time: 13 ms (60.77%), Space: 7.4 MB (48.35%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        
+        vector<int> count (26);
+        
+        for (int i {0}; i < s.length(); ++i)
+        {
+            ++count[s[i] - 'a'];
+        }
+        
+        for (int j {0}; j < t.length(); ++j)
+        {
+            --count[t[j] - 'a'];
+            if (count[t[j] - 'a'] < 0)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
