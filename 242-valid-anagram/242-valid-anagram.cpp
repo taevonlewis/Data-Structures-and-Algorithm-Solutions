c1674364f374814fa826d07a7f49dd767d8fe227
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-09-06 15:08:41 -0400

    Time: 32 ms (5.66%), Space: 7.4 MB (8.86%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,17 +3,17 @@ public:
     bool isAnagram(string s, string t) {
         if (s.length() != t.length()) return false;
         
-        vector<int> count (26);
+        unordered_map<char, int> mp;
         
         for (int i {0}; i < s.length(); ++i)
         {
-            ++count[s[i] - 'a'];
+            ++mp[s[i] - 'a'];
         }
         
         for (int j {0}; j < t.length(); ++j)
         {
-            --count[t[j] - 'a'];
-            if (count[t[j] - 'a'] < 0)
+            --mp[t[j] - 'a'];
+            if (mp[t[j] - 'a'] < 0)
             {
                 return false;
             }
