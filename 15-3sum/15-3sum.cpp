f22073f897610af6c2f0fa94220134ad2419bb63
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-09-26 13:33:19 -0400

    Time: 77 ms (96.38%), Space: 19.8 MB (95.55%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -5,39 +5,41 @@ public:
         int sum {0};
         
         sort(nums.begin(), nums.end());
+        // -4, -1, -1, 0, 1, 2
         
         for (int i {0}; i < nums.size(); ++i)
         {
             if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i - 1] == nums[i]) continue;
             
-            int j = i + 1;
+            int j { i + 1 };
             int k = nums.size() - 1;
             
             while (j < k)
             {
                 sum = nums[i] + nums[j] + nums[k];
                 
-                if (sum > 0)
-                {
-                    --k;
-                }
-                else if (sum < 0)
+                if (sum < 0)
                 {
                     ++j;
                 }
-                else {
+                else if (sum > 0)
+                {
+                    --k;
+                }
+                else
+                {
                     answer.push_back({nums[i], nums[j], nums[k]});
                     
-                    int jDupe = nums[j];
-                    int kDupe = nums[k];
+                    int jDupe { nums[j] };
+                    int kDupe { nums[k] };
                     
-                    while (j < k && nums[j] == jDupe)
+                    while (j < k && jDupe == nums[j])
                     {
                         ++j;
                     }
                     
-                    while (j < k && nums[k] == kDupe)
+                    while (j < k && kDupe == nums[k])
                     {
                         --k;
                     }
