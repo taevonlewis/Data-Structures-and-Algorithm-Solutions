e79704b9e6f686d492b00f3574902ca73f9ecec7
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-10-03 12:40:25 -0400

    Time: 307 ms (5.03%), Space: 30.9 MB (5.83%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
-        map<map<char, int>, vector<string>> mp;
-        vector<vector<string>>ans;
+        unordered_map<string, vector<string>> mp;
         
-        int i=0;
-        while(i<n){
-            map<char, int> a;
-            for(int j=0; j<strs[i].size(); j++)
-                a[strs[i][j]]++;
-            mp[a].push_back(strs[i]);
-            i++;
+        for (int i {0}; i < strs.size(); ++i)
+        {
+            std::string key = getKey(strs[i]);
+            mp[key].push_back(strs[i]);
         }
-        for(auto i=mp.begin(); i!= mp.end(); i++){
-            ans.push_back(i->second);
-        }
-        return ans;
         
+        vector<vector<string>> anagrams;
+        for (auto i : mp)
+        {
+            anagrams.push_back(i.second);
+        }
+        
+        return anagrams;
+    }
+    
+private:
+    std::string getKey(std::string str) {
+        vector<int> count(26);
+            
+        for (int i {0}; i < str.size(); ++i)
+        {
+            ++count[str[i] - 'a'];
+        }
+            
+        std::string key {""};
+        for (int i {0}; i < 26; ++i)
+        {
+            key.append(to_string(count[i] + 'a'));
+        }
+        
+        return key;
     }
 };
