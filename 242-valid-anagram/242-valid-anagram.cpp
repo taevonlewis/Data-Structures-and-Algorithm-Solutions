259fc5bf577c783b88d62247da9cbc9b27fe8ff9
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-10-19 12:55:36 -0400

    Time: 3 ms (99.27%), Space: 7.4 MB (13.42%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        /*
+        edge case: if the first string's size != second string's size, return false
+        create a vector of 26 integers (entire lowercase alphabet)
+            to store each char at it's respective index
+                and increase it's count
+                EX. 'e' - 'a' = 5 - 0 = index 5
+        loop through first string
+            add each chars index and increase count in vector
+        loop through second string
+            add each chars index and decrease count in vector
+            if each chars count is less than 0, return false
+        return true
+        */
+        
         if (s.length() != t.length()) return false;
         
         vector<int> count(26);
@@ -14,10 +28,7 @@ public:
         {
             --count[t[j] - 'a'];
             
-            if (count[t[j] - 'a'] < 0)
-            {
-                return false;
-            }
+            if (count[t[j] - 'a'] < 0) return false;
         }
         
         return true;
