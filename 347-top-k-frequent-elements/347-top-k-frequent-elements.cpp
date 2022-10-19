11a994c072322fec95c084299e5f60e3d33f4ea9
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-10-19 14:06:49 -0400

    Time: 42 ms (18.38%), Space: 15.5 MB (7.93%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
+        /*
+        create an unordered_map with int key (number) and int value (count)
+        loop through each number
+            add each number to map and increase its count
+        create a vector of int vector (bucket) using the same size as nums vector + 1
+            // +1 because of 0 indexing, we need it 1 indexing
+                // 1 indexing because we're gonna use the count as key
+                // EX. bucket 3 has a 1 inside since 1 occurs 3 times
+            from the map, push each number (key) inside count (value) which creates buckets
+        create a final result int vector
+        loop through nums vector in reverse
+            // we want the most occurred numbers first based on how many numbers we need
+            if the result vector is >= in size than k (most frequent numbers we're looking for), break
+            if the buckets vector of int vectors is not empty (the bucket has (a) value(s))
+                insert each value from bucket (beginning to end) to end of result vector
+        return result
+        */
+        
+        unordered_map<int, int> numsCount;
         for (int i {0}; i < nums.size(); ++i)
         {
-            ++mp[nums[i]];
+            ++numsCount[nums[i]];
         }
         
         vector<vector<int>> buckets(nums.size() + 1);
-        for (auto i : mp)
+        for (auto it = numsCount.begin(); it != numsCount.end(); ++it)
         {
-            buckets[i.second].push_back(i.first);
+            buckets[it->second].push_back(it->first);
         }
         
-        vector<int> results;
-        for (int i = buckets.size() - 1; i >= 0; --i)
+        vector<int> result;
+        for (int i = nums.size(); i >= 0; --i)
         {
-            if (results.size() >= k) break;
+            if (result.size() >= k) break;
             
             if (!buckets[i].empty())
             {
-                results.insert(results.end(), buckets[i].begin(), buckets[i].end());
+                result.insert(result.end(), buckets[i].begin(), buckets[i].end());
             }
         }
         
-        return results;
+        return result;
     }
 };
