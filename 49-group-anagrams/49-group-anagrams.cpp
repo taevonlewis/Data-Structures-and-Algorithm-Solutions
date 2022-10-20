7812cf6400aaffbe621f9526b5d91ee12a86a182
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-10-20 10:58:25 -0400

    Time: 300 ms (5.77%), Space: 27.9 MB (8.96%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,38 +1,15 @@
-/*
-    create a private deconstructor
-        create a getKey string function that'll pass a string to groupAnagrams function
-            create an 26 int vector to store each char's index and count
-            loop through string
-                add each char's index and increase count in vector
-            create a string key that holds an empty string
-            loop from index 0 - 26
-                append the stringed char index from vector to key
-            return string key
-    
-    groupAnagrams
-        create an unordered_map to hold a string (key) and vector of strings (anagrams)
-        loop through each string in string vector
-            create a string key to hold the create key in the getKey function
-                this will be the key or index of each string that is an anagram of another
-            push in each string into the map given its key
-        create the returned vector of vector strings
-        loop through map
-            push in each string (value) into vector of vector strings
-        return vector of vector strings
-*/
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> tempAnagrams;
+        unordered_map<string, vector<string>> tempAna;
         for (int i {0}; i < strs.size(); ++i)
         {
             std::string key = getKey(strs[i]);
-            tempAnagrams[key].push_back(strs[i]);
+            tempAna[key].push_back(strs[i]);
         }
         
         vector<vector<string>> anagrams;
-        for (auto it = tempAnagrams.begin(); it != tempAnagrams.end(); ++it)
+        for (auto it = tempAna.begin(); it != tempAna.end(); ++it)
         {
             anagrams.push_back(it->second);
         }
