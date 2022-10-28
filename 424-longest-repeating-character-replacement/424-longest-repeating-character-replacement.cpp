b4bc997ec4c530c6d7c62b2df0dcda8ad646b28c
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-10-28 12:47:37 -0400

    Time: 6 ms (95.30%), Space: 7.1 MB (53.04%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,26 +1,10 @@
-/*
-    create int vector of 26 to count amount of each char (26 uppercase chars in alphabet)
-    create int result to store longest substring length
-    create int maxCount to store the most frequent char
-    create i and j iterators, both at 0
-    while loop: from j to string length
-        increase the count of each char at it's respective index in the alphabet
-        set maxCount to the max between maxcount and most frequent char
-        if (the window length - most frequent chars > k)
-            // in any given window, if the changes needed are more than changes allowed (k)
-            decrease the count for the char
-            reduce window length, ++i
-        set result to the max between result and the current window length
-        increase window length, ++j
-    return result
-*/
-
 class Solution {
 public:
     int characterReplacement(string s, int k) {
         vector<int> count(26);
-        int result {0};
         int maxCount {0};
+        int result {0};
+        
         int i {0};
         int j {0};
         
