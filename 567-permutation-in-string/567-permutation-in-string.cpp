99382af3f1b8c4891de4d22ae5097ceff9e32d96
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-10-28 13:04:33 -0400

    Time: 25 ms (40.54%), Space: 18.6 MB (16.46%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,27 +1,3 @@
-/*
-    public:
-        base case: if the first string has more chars than the second, return false
-        create int vector count of 26 to hold char occurrences at alphabetic index (26 lowercase letters in alphabet)
-        loop thru first string
-            increase char count of first string
-            decrease char count of second string
-            // basically, we want to check if the count of each char is even at 0
-        conditional: call to ispermu function to check if the count is at 0 or not
-            return true
-        loop thru second string from first string length to second string length
-            decrease each char's count
-            if each char's count does NOT equal 0
-                return false
-        return true
-    
-    private:
-        create ispermu function that holds a int vector
-            loop from 0 to 26
-                if each char's count does NOT equal 0
-                    return false
-            return true
-*/
-
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
@@ -53,9 +29,8 @@ public:
         return false;
     }
     
-private:
     bool isPermu(vector<int> count) {
-        for (int i {0}; i < 26; ++i)
+        for (int i {0}; i < count.size(); ++i)
         {
             if (count[i] != 0) return false;
         }
