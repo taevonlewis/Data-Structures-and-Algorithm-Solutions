130677a97aaa7aa4c0dc02977e01c4a597d24b7b
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-11-17 12:24:15 -0500

    Time: 22 ms (13.46%), Space: 14.8 MB (81.72%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -18,27 +18,23 @@ public:
         if (list2 == NULL) return list1;
         
         ListNode* head { NULL };
-        
         if (list1->val <= list2->val)
         {
             head = list1;
             list1 = list1->next;
-        }
-        else {
+        } else {
             head = list2;
             list2 = list2->next;
         }
         
         ListNode* curr { head };
-        
         while (list1 != NULL && list2 != NULL)
         {
             if (list1->val <= list2->val)
             {
                 curr->next = list1;
                 list1 = list1->next;
-            }
-            else {
+            } else {
                 curr->next = list2;
                 list2 = list2->next;
             }
@@ -49,8 +45,7 @@ public:
         if (list1 == NULL)
         {
             curr->next = list2;
-        }
-        else {
+        } else {
             curr->next = list1;
         }
         
