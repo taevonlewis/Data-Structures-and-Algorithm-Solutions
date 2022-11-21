4720f444b7f5823cf09b26814f1feb2b9f478421
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-11-21 12:42:59 -0500

    Time: 4 ms (54.86%), Space: 9.6 MB (79.48%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -13,11 +13,9 @@ class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
         if (root == NULL)
-        return NULL ;
+        return NULL;
         
-        TreeNode* temp = root->left;
-        root->left = root->right;
-        root->right = temp;
+        swap(root->left, root->right);
         
         invertTree(root->left);
         invertTree(root->right);
