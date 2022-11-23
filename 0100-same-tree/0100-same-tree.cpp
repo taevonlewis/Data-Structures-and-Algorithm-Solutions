8cf66ceb4a7d0519ac5737d4143ad60a3264495f
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-11-22 19:48:46 -0500

    Time: 4 ms (47.68%), Space: 9.9 MB (77.92%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -13,15 +13,15 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         //      RECURSIVE SOLUTION
-//         if (p == NULL && q == NULL) return true;
+        if (p == NULL && q == NULL) return true;
         
-//         if (p == NULL || q == NULL) return false;
+        if (p == NULL || q == NULL) return false;
         
-//         if (p->val != q->val) return false;
+        if (p->val != q->val) return false;
         
-//         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
         
-        //      ITERATIVE SOLUTION
+        /*      ITERATIVE SOLUTION
         queue<TreeNode*> treeQueue;
         treeQueue.push(p);
         treeQueue.push(q);
@@ -47,5 +47,6 @@ public:
         }
         
         return true;
+        */
     }
 };
