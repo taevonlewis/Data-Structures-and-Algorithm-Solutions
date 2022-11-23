aa385e43d9d89a0d36e1a497a7bf5eccaa58172f
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-11-22 20:53:22 -0500

    Time: 31 ms (24.69%), Space: 21.7 MB (31.94%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -10,32 +10,61 @@
  * };
  */
 class Solution {
+    //      RECURSIVE SOLUTION
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         TreeNode* prev { NULL };
+        
+//         return validateNodes(root, prev);
+//     }
+    
+// private:
+//     bool validateNodes(TreeNode* root, TreeNode*& prev) {
+//         if (root == NULL) return true;
+        
+//         if (!validateNodes(root->left, prev))
+//         {
+//             return false;
+//         }
+        
+//         if (prev != NULL && prev->val >= root->val)
+//         {
+//             return false;
+//         }
+        
+//         prev = root;
+        
+//         if (!validateNodes(root->right, prev))
+//         {
+//             return false;
+//         }
+        
+//         return true;
+//     }
+    
 public:
     bool isValidBST(TreeNode* root) {
+        stack<TreeNode*> rootStack;
         TreeNode* prev { NULL };
         
-        return validateNodes(root, prev);
-    }
-    
-private:
-    bool validateNodes(TreeNode* root, TreeNode*& prev) {
-        if (root == NULL) return true;
-        
-        if (!validateNodes(root->left, prev))
+        while (!rootStack.empty() || root != NULL)
         {
-            return false;
-        }
-        
-        if (prev != NULL && prev->val >= root->val)
-        {
-            return false;
-        }
-        
-        prev = root;
-        
-        if (!validateNodes(root->right, prev))
-        {
-            return false;
+            while (root != NULL)
+            {
+                rootStack.push(root);
+                root = root->left;
+            }
+            
+            root = rootStack.top();
+            rootStack.pop();
+            
+            if (prev != NULL && prev->val >= root->val)
+            {
+                return false;
+            }
+            
+            prev = root;
+            root = root->right;
         }
         
         return true;
