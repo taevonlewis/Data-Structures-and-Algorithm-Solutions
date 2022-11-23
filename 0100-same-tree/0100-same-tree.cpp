97cbe73e1b7256d8aec1d349eb88302ca468cc5a
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-11-22 19:48:06 -0500

    Time: 8 ms (14.91%), Space: 10 MB (44.37%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,12 +12,40 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q == NULL) return true;
+        //      RECURSIVE SOLUTION
+//         if (p == NULL && q == NULL) return true;
         
-        if (p == NULL || q == NULL) return false;
+//         if (p == NULL || q == NULL) return false;
         
-        if (p->val != q->val) return false;
+//         if (p->val != q->val) return false;
         
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+//         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        
+        //      ITERATIVE SOLUTION
+        queue<TreeNode*> treeQueue;
+        treeQueue.push(p);
+        treeQueue.push(q);
+        
+        while (!treeQueue.empty())
+        {
+            TreeNode* pQueue { treeQueue.front() };
+            treeQueue.pop();
+            
+            TreeNode* qQueue { treeQueue.front() };
+            treeQueue.pop();
+            
+            if (pQueue == NULL && qQueue == NULL) continue;
+            
+            if (pQueue == NULL || qQueue == NULL) return false;
+            
+            if (pQueue->val != qQueue->val) return false;
+            
+            treeQueue.push(pQueue->left);
+            treeQueue.push(qQueue->left);
+            treeQueue.push(pQueue->right);
+            treeQueue.push(qQueue->right);
+        }
+        
+        return true;
     }
 };
