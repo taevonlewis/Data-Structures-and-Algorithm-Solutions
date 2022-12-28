1cb9c899f480bab5eb1ebeb32737ce894321c18d
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-12-28 15:17:02 -0500

    Time: 11 ms (62.85%), Space: 13.3 MB (33.43%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int result = 1;
+        
+        for (int i = 0; i < s.size() - 1; i++) {
+            middleOut(s, i, i, result);
+            middleOut(s, i, i + 1, result);
+        }
+        
+        return result;
+    }
+private:
+    void middleOut(string s, int i, int j, int& result) {
+        while (i >= 0 && j < s.size() && s[i] == s[j]) {
+            result++;
+            i--;
+            j++;
+        }
+    }
+};
