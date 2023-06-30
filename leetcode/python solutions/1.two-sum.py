a8e126cb01e2df3c33187454d4651b8984dc2977
taevonlewis <lewistpro@gmail.com>
2023-06-29 20:35:34 -0400

    added two sum

---
+++ leetcode/python solutions/1.two-sum.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in map:
+                return [map[target - nums[i]], i]
+            else:
+                map[nums[i]] = i
+# @lc code=end
+
