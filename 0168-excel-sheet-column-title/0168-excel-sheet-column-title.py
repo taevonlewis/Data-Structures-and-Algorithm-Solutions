1eec07e5b265261db6bbfcce2f948149b7867933
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-07-03 17:49:21 -0400

    Time: 56 ms (5.58%), Space: 16.4 MB (24.52%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -1,6 +1,6 @@
 """
 Problem Summary: Using base conversion (specifically base-26) from the column number to it's character representation
-Pattern/Approach: No pattern, mathematical computation
+Pattern/Approach: Base conversion, base-10 to base-26
 Input: 701
 Output: "ZY"
 Assumptions/Constraints: input will be 1+ 
@@ -8,12 +8,12 @@ Processes/Steps:
     create alphabet string containing each char
     create res string
     while num != 0
-        decrement num by 1 (cause need 0-index based)
+        decrement num by 1 to account for 0-indexing
         set index to num remainder
         prepend alphabet[num] + res
-        set num to num // 26
+        update num to num // 26
     return res
-Edge Cases: 
+Edge Cases: if input is valid
 """
 
 class Solution:
