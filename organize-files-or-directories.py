e99bd4c4e06209265e1190d25a0796de376d05c8
taevonlewis <lewistpro@gmail.com>
2023-07-04 11:17:07 -0400

    modified organized_directories function

--- organize-files-or-directories.py
+++ organize-files-or-directories.py
@@ -7,16 +7,25 @@ def organize_directories(directory):
             # Split the filename on the hyphen
             split_filename = filename.split('-', 1)
             
-            # Check if the split was successful
-            if len(split_filename) > 1:
+            # Check if the split was successful and the first part is a number
+            if len(split_filename) > 1 and split_filename[0].isdigit():
                 # Create the new directory name
                 new_directory = os.path.join(directory, split_filename[1])
 
                 # Create the new directory if it doesn't already exist
                 os.makedirs(new_directory, exist_ok=True)
 
-                # Move the old directory to the new directory
-                shutil.move(os.path.join(directory, filename), os.path.join(new_directory, filename))
+                # If a directory with the same name doesn't already exist in the new directory, move the old directory
+                if not os.path.exists(os.path.join(new_directory, filename)):
+                    shutil.move(os.path.join(directory, filename), os.path.join(new_directory, filename))
+                else:
+                    # Otherwise, move the files from the old directory to the existing directory
+                    for file in os.listdir(os.path.join(directory, filename)):
+                        shutil.move(os.path.join(directory, filename, file), os.path.join(new_directory, file))
+
+                    # If the old directory is empty after moving the files, remove it
+                    if not os.listdir(os.path.join(directory, filename)):
+                        os.rmdir(os.path.join(directory, filename))
 
 def organize_files(directory):
     for filename in os.listdir(directory):
@@ -37,4 +46,4 @@ def organize_files(directory):
 
 
 # organize_files('/Users/tizzymatic/Documents/GitHub/Data-Structures-and-Algorithm-Solutions/hackerrank')
-# organize_directories('/Users/tizzymatic/Documents/GitHub/Data-Structures-and-Algorithm-Solutions/leetcode/python')
+organize_directories('/Users/tizzymatic/Documents/GitHub/Data-Structures-and-Algorithm-Solutions/leetcode/python')
