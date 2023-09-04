002520b7e8c97fe5abb259c021fb8a0a7622e322
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-09-04 11:38:02 -0400

    Time: 56 ms (42.37%), Space: 14.2 MB (75.95%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.swift
+++ 2058-concatenation-of-array/2058-concatenation-of-array.swift
@@ -1,11 +1,5 @@
 class Solution {
     func getConcatenation(_ nums: [Int]) -> [Int] {
-        var arr = nums
-
-        for i in nums {
-            arr.append(i)
-        }
-
-        return arr
+        nums + nums
     }
 }
