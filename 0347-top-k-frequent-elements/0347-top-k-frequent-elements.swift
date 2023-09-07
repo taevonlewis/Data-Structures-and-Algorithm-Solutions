0f49ea44ec6bc5da5596df5d7ee7f83345553f42
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-09-07 11:46:48 -0400

    Time: 75 ms (25.9%), Space: 15.6 MB (86.2%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func topKFrequent(_ nums: [Int], _ k: Int) ->  [Int] {
+        var map: [Int: Int] = [:]
+        for num in nums {
+            map[num, default: 0] += 1
+        }
+
+        var buckets: [[Int]] = [[Int]](repeating: [], count: nums.count + 1)
+        for (key, val) in map {
+            buckets[val].append(key)
+        }
+
+        var res: [Int] = []
+        for i in stride(from: buckets.count - 1, through: 0, by: -1) {
+            if res.count == k {
+                break
+            }
+
+            res += buckets[i]
+        }
+
+        return res
+    }
+}
