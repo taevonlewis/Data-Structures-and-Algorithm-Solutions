8fd3559565034849a12a2a87cc839bb659ab60e9
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-09-08 13:08:00 -0400

    Time: 6 ms (95.3%), Space: 14 MB (93.67%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        if strs.isEmpty {
+            return ""
+        }
+
+        let arr = strs.map { Array($0) }
+        let firstStr = arr[0]
+
+        for i in 0..<firstStr.count {
+            let char = firstStr[i]
+
+            for j in 1..<arr.count {
+                if i == arr[j].count || arr[j][i] != char {
+                    return String(firstStr[0..<i])
+                }
+            }
+        }
+
+        return String(firstStr)
+    }
+}
