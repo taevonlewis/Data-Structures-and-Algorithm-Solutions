4b31d66fc88cc982ea3f482239155a03414cd4a6
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-09-08 11:23:57 -0400

    Time: 12 ms (77.55%), Space: 14.3 MB (50.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.swift
+++ 0013-roman-to-integer/0013-roman-to-integer.swift
@@ -1,23 +1,28 @@
 class Solution {
     func romanToInt(_ s: String) -> Int {
-        var roman: [Character: Int] = ["I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000]
-        var res: Int = 0
+        let roman: [Character: Int] = [
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        ]
         var arr = Array(s)
-
+        var res: Int = 0
         if let val = roman[arr[0]] {
-            res = val
+            res += val
         }
 
         for i in 1..<arr.count {
             let prev = roman[arr[i - 1]]
             let curr = roman[arr[i]]
 
-            if prev != nil, curr != nil {
-                if prev! < curr! {
-                    res += curr! - (2 * prev!)
-                } else {
-                    res += curr!
-                }
+            if prev != nil, curr != nil, prev! < curr! {
+                res += curr! - (2 * prev!)
+            } else {
+                res += curr!
             }
         }
 
