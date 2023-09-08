772fe2f05034d1e2bfc3179c38a0f31d2620f857
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2023-09-08 16:38:38 -0400

    Time: 794 ms (34.72%), Space: 17.8 MB (59.33%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.swift
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.swift
@@ -1,8 +1,8 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
-        var i = 0
-        var j = 1
-        var maxProfit = 0
+        var maxProfit: Int = 0
+        var i: Int = 0
+        var j: Int = 1
 
         while j < prices.count {
             if prices[i] < prices[j] {
@@ -14,6 +14,7 @@ class Solution {
 
             j += 1
         }
+
         return maxProfit
     }
 }
