330cd9ae857609c6d02b6c1674e3bdef89e56964
TaeVon Lewis <65370736+taevonlewis@users.noreply.github.com>
2024-07-07 16:03:53 -0400

    add validParentheses

---
+++ fully-written-solutions/practice/swift/validParentheses.swift
@@ -0,0 +1,23 @@
+func isValid(_ str: String) -> Bool {
+    var st: [Character] = []
+
+    for char in str {
+        if char == "(" || char == "[" || char == "{" {
+            st.append(char)
+        } else {
+            if st.isEmpty {
+                return false
+            }
+
+            if char == ")" && st.removeLast() != "(" || char == "]" && st.removeLast() != "[" || char == "}" && st.removeLast() != "{" {
+                return false
+            }
+        }
+    }
+
+    return st.isEmpty
+}
+
+print(isValid("()[]{}"))
+print(isValid("([{}])"))
+print(isValid("(]"))
